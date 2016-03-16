6e5c3c39529d62847878c0ac6ce5d83bb1c04719
Antonio Sejas <antonio@sejas.es>
2016-03-16 00:46:21 +0000

    change name and subtitle in detail page

--- 01-TableView/01-TableView/DetailViewController.swift
+++ 01-TableView/01-TableView/DetailViewController.swift
@@ -10,26 +10,15 @@ import UIKit
 
 class DetailViewController: UIViewController {
 
+    @IBOutlet weak var lblName: UILabel!
+    @IBOutlet weak var lblSubtitle: UILabel!
+    var name = "Sejas"
+    var subtitle = "iOS Instructor"
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        self.lblName.text = self.name
+        self.lblSubtitle.text = self.subtitle
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
