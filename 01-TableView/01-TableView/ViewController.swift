2cdda37ce080d0572c8fe1a07d495630fd11f4c0
Antonio Sejas <antonio@sejas.es>
2016-03-16 00:19:42 +0000

    table view working, background yellow and text row i, files

---
+++ 01-TableView/01-TableView/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  01-TableView
+//
+//  Created by Antonio Sejas on 15/3/16.
+//  Copyright © 2016 Antonio Sejas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        self.tableView.dataSource = self
+        self.tableView.delegate = self
+        
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.backgroundColor = UIColor.yellowColor()
+        cell.textLabel!.text = "ROW: \(indexPath.row)"
+        return cell
+    }
+}
+
